53bb32c26c32188c95940641ea3ff60181c16388
Melvin Thomas <melvinchooranolil@gmail.com>
2022-09-10 12:00:12 +0530

    space refactoring

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,40 +1,42 @@
-import { MoreVert } from '@material-ui/icons'
-import "./post.css"
-import { Users } from "../../dummyData"
+import { MoreVert } from "@material-ui/icons";
+import "./post.css";
+import { Users } from "../../dummyData";
 
-
-export default function Post({post}) {
-    console.log(post.img)
-    const user = Users.filter((u) => u.id === post.userId)[0]
+export default function Post({ post }) {
+  console.log(post.img);
+  const user = Users.filter((u) => u.id === post.userId)[0];
 
   return (
-    <div className='post'>
-        <div className='postWrapper'>
-            <div className='postTop'>
-                <div className='postTopLeft'>
-                    <img className='postProfileImg' src={user.profilePicture} alt=''/>
-                    <span className='postUsername'>{user.username}</span>
-                    <span className='postDate'>{post.date} </span>
-                </div>
-                <div className='postTopRight'>
-                    <MoreVert />
-                </div>
-            </div>
-            <div className='postCenter'>
-                <span className='postText'>{post?.desc}</span>
-                <img className='postImg' src={post.photo} alt=''/>
-            </div>
-            <div className='postBottom'>
-                <div className='postBottomLeft'>
-                    <img className="likeIcon" src='/assets/like.png' alt=''/>
-                    <img className="likeIcon" src='/assets/heart.png' alt=''/>
-                    <span className='postLikeCounter'> {post.like} people like this</span>
-                </div>
-                <div className='postBottomRight'>
-                    <span className='postCommentText'>{post.comment} Comments</span>
-                </div>
-            </div>
-        </div>        
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img className="postProfileImg" src={user.profilePicture} alt="" />
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{post.date} </span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">
+              {" "}
+              {post.like} people like this
+            </span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} Comments</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
