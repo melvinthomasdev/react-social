0cb6e278b59b6dae3fdebe73aa47fc2531e64953
Melvin Thomas <melvinchooranolil@gmail.com>
2022-09-10 12:00:11 +0530

    function arguement fix

--- src/components/onlineFriend/OnlineFriend.jsx
+++ src/components/onlineFriend/OnlineFriend.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./onlineFriend.css"
 
-export default function OnlineFriend(user) {
+export default function OnlineFriend({user}) {
   return (
     <div>
       <li className="rightbarFriend">
         <div className="rightbarProfileImgContainer">
           <img
             className="rightbarProfileImg"
-            src={user.user.profilePicture}
+            src={user.profilePicture}
             alt=""
           />
           <span className="rightbarOnlineBadge"></span>
         </div>
-        <span className="rightbarUsername">{user.user.username} </span>
+        <span className="rightbarUsername">{user.username} </span>
       </li>
     </div>
   );
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -3,9 +3,9 @@ import "./post.css"
 import { Users } from "../../dummyData"
 
 
-export default function Post(post) {
-    console.log(post.post.img)
-    const user = Users.filter((u) => u.id === post.post.userId)[0]
+export default function Post({post}) {
+    console.log(post.img)
+    const user = Users.filter((u) => u.id === post.userId)[0]
 
   return (
     <div className='post'>
@@ -14,24 +14,24 @@ export default function Post(post) {
                 <div className='postTopLeft'>
                     <img className='postProfileImg' src={user.profilePicture} alt=''/>
                     <span className='postUsername'>{user.username}</span>
-                    <span className='postDate'>{post.post.date} </span>
+                    <span className='postDate'>{post.date} </span>
                 </div>
                 <div className='postTopRight'>
                     <MoreVert />
                 </div>
             </div>
             <div className='postCenter'>
-                <span className='postText'>{post.post?.desc}</span>
-                <img className='postImg' src={post.post.photo} alt=''/>
+                <span className='postText'>{post?.desc}</span>
+                <img className='postImg' src={post.photo} alt=''/>
             </div>
             <div className='postBottom'>
                 <div className='postBottomLeft'>
                     <img className="likeIcon" src='/assets/like.png' alt=''/>
                     <img className="likeIcon" src='/assets/heart.png' alt=''/>
-                    <span className='postLikeCounter'> {post.post.like} people like this</span>
+                    <span className='postLikeCounter'> {post.like} people like this</span>
                 </div>
                 <div className='postBottomRight'>
-                    <span className='postCommentText'>{post.post.comment} Comments</span>
+                    <span className='postCommentText'>{post.comment} Comments</span>
                 </div>
             </div>
         </div>        
