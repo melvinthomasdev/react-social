f386157753a874de3a8d7dfad3cb6377afe45174
Melvin Thomas <melvinchooranolil@gmail.com>
2022-09-10 12:00:14 +0530

    unwanted imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Profile from "./pages/profile/Profile";
 
 
 function App() {
-  return <Profile />
+  return <Home />
 }
 
 export default App;
